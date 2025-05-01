516f00a2e640719880a5720df5a5f2c85eb316dc
DaveTheEpic04 <83246071+DaveTheEpic04@users.noreply.github.com>
2025-05-01 16:38:53 +0100

    Update ActivityGraphs.py

--- ActivityGraphs.py
+++ ActivityGraphs.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 with open("text.json", "r") as f:
     data = json.loads(f.read())
 
-game = "karlson_itch_io"
-category = ""
-isLevel = True
-level = "Escape 2"
+game = "karlson_itch_io" # Name of the game being analysed
+category = "" # Category of the game, leave blank for all categories
+isLevel = True # True if a category is for Individual Levels, False for Full Game Categories
+level = "Escape 2" # Level of the game, leave blank for all levels
 
 gameData = srcpy.GetGameData(gameUrl=game).perform()
 
